5e5b2d44042a4856108eb9c29d0ad14ca1da1fd9
okame-486 <okame.aka.k3@gmail.com>
2023-12-24 17:34:25 +0900

    SneakListenerをリファクタリングしてCooldownHandlerを作成使用し、成長ロジックを改善 - HashMapの代わりにCooldownHandlerをインポートしてクールダウンを管理 - サトウキビの成長ロジックを追加 - 農地の作物とサトウキビの成長ロジックをリファクタリング - 成長とパーティクル効果のための追加のチェックと条件を追加 - 未使用の変数とインポートを削除

--- src/main/kotlin/com/okamesvr/growthpromotant/SneakListener.kt
+++ src/main/kotlin/com/okamesvr/growthpromotant/SneakListener.kt
@@ -1,5 +1,6 @@
 package com.okamesvr.growthpromotant
 
+import com.okamesvr.growthpromotant.utils.CooldownHandler
 import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.Sound
@@ -7,39 +8,32 @@ import org.bukkit.block.data.Ageable
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerToggleSneakEvent
-import java.util.UUID
 import kotlin.random.Random
 
 class SneakListener : Listener {
     companion object {
         var PROBABILITY = 0.2
-        var COOLTIME = 0
         var BoneParticle = true
         var CompleteGrowthParticle = true
         var BoneSound = true
         var FullGrowthSound = true
-        private val sneakCooldown = HashMap<UUID, Long>()
+        private val cooldownHandler = CooldownHandler()
     }
     @EventHandler
     fun onPlayerToggleSneak(event: PlayerToggleSneakEvent) {
         val player = event.player
         val playerLocation = player.location
-        val block = playerLocation.subtract(0.0, 0.9375, 0.0).block
+        val farmlandBlock = playerLocation.subtract(0.0, 0.9375, 0.0).block
+        val block = playerLocation.subtract(0.0, -1.0, 0.0).block
 
-
-        if (block.type == Material.FARMLAND) {
+        // 農地作物の処理
+        if (farmlandBlock.type == Material.FARMLAND) {
             // クールタイム処理
-            val currentTime = System.currentTimeMillis()
-
-            if (sneakCooldown.containsKey(player.uniqueId)) {
-                val lastSneakTime = sneakCooldown[player.uniqueId]
-                if (lastSneakTime != null && currentTime - lastSneakTime < COOLTIME) {
-                    return
-                }
+            if (cooldownHandler.isOnCooldown(player.uniqueId)) {
+                return
             }
-            sneakCooldown[player.uniqueId] = currentTime
 
-            val aboveBlock = block.location.add(0.0, 1.0, 0.0).block
+            val aboveBlock = farmlandBlock.location.add(0.0, 1.0, 0.0).block
             val blockData = aboveBlock.blockData
 
             // 確率&成長処理
@@ -59,6 +53,66 @@ class SneakListener : Listener {
                     }
 
                     if (blockData.age == blockData.maximumAge) {
+                        if (FullGrowthSound) {
+                            player.playSound(playerLocation, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.5f)
+                        }
+                        if (CompleteGrowthParticle) {
+                            player.world.spawnParticle(Particle.END_ROD, endRodLocation, 30, 0.2, 0.3, 0.2, 0.03)
+                        }
+                    }
+                }
+            }
+        }
+        // サトウキビの処理
+        if (block.type == Material.SUGAR_CANE) {
+
+            // クールタイム処理
+            if (cooldownHandler.isOnCooldown(player.uniqueId)) {
+                return
+            }
+
+            // ブロックデータの取得
+            val aboveBlock = block.location.add(0.0, 0.0, 0.0).block
+            val blockData = aboveBlock.blockData
+
+            if (blockData is Ageable) {
+
+                val topBlock = aboveBlock.location.add(0.0, 1.0, 0.0).block
+                val topTopBlock = topBlock.location.add(0.0, 1.0, 0.0).block
+
+                // 成長のリセット処理
+                if (blockData.age == 15 && topTopBlock.type == Material.AIR) {
+                    if (topBlock.type == Material.AIR || topBlock.type == Material.SUGAR_CANE) {
+                        blockData.age = 0
+                    }
+                }
+
+                if (blockData.age < blockData.maximumAge && Random.nextDouble() < PROBABILITY) {
+                    blockData.age += 1
+                    aboveBlock.blockData = blockData
+
+                    val particleLocation = aboveBlock.location.add(0.5, 0.1, 0.5)
+                    val endRodLocation = aboveBlock.location.add(0.5, 0.0, 0.5)
+
+                    if (BoneParticle) {
+                        player.world.spawnParticle(Particle.VILLAGER_HAPPY, particleLocation, 8, 0.2, 0.0, 0.2)
+                    }
+                    if (BoneSound) {
+                        player.playSound(playerLocation, Sound.ITEM_BONE_MEAL_USE, 1.0f, 1.5f)
+                    }
+
+                    if (blockData.age == blockData.maximumAge) {
+                        // 修正必須
+                        if (topBlock.type == Material.SUGAR_CANE && topTopBlock.type != Material.SUGAR_CANE) {
+                            topTopBlock.type = Material.SUGAR_CANE
+                            blockData.age = 15
+                            aboveBlock.blockData = blockData
+                        } else if (topBlock.type != Material.SUGAR_CANE && topTopBlock.type != Material.SUGAR_CANE) {
+                            topBlock.type = Material.SUGAR_CANE
+                            blockData.age = 0
+                            aboveBlock.blockData = blockData
+                        }
+
                         if (FullGrowthSound) {
                             player.playSound(playerLocation, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.5f)
                         }
