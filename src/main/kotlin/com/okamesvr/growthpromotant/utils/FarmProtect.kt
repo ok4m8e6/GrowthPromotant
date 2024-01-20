15d47d8425542ed7cf83b79d443a7256ddee71ef
okame-486 <okame.aka.k3@gmail.com>
2024-01-20 14:06:33 +0900

    FarmProtect： 動物が農地を踏み荒らすのを防ぐように修正 - 動物が農地を踏み荒らすのを防ぐ新しいイベントハンドラを追加しました。この機能により、ゲーム内の農地の保護メカニズムが強化されました。

--- src/main/kotlin/com/okamesvr/growthpromotant/utils/FarmProtect.kt
+++ src/main/kotlin/com/okamesvr/growthpromotant/utils/FarmProtect.kt
@@ -1,9 +1,11 @@
 package com.okamesvr.growthpromotant.utils
 
 import org.bukkit.Material
+import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.event.player.PlayerInteractEvent
 
 class FarmProtect : Listener {
@@ -18,5 +20,11 @@ class FarmProtect : Listener {
             }
         }
     }
-    
+    @EventHandler
+    fun onAnimalTrampling(event: EntityChangeBlockEvent) {
+        val entity = event.entity
+        if (entity is LivingEntity && event.block.type == Material.FARMLAND) {
+            event.isCancelled = true
+        }
+    }
 }
