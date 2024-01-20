2553631e416072a5fc3f0c10b6634ddabedcb17c
okame-486 <okame.aka.k3@gmail.com>
2024-01-20 13:52:35 +0900

    EffectsLib.kt - パーティクルとサウンドエフェクトを扱うEffectsLib.ktを追加

---
+++ src/main/kotlin/com/okamesvr/growthpromotant/utils/EffectsLib.kt
@@ -0,0 +1,42 @@
+package com.okamesvr.growthpromotant.utils
+
+import org.bukkit.*
+import org.bukkit.entity.Player
+
+class EffectsLib {
+    companion object {
+        var BoneParticle = true
+        var CompleteGrowthParticle = true
+        var BoneSound = true
+        var FullGrowthSound = true
+
+        val BoneParticleType = Particle.VILLAGER_HAPPY
+        val CompleteGrowthParticleType = Particle.END_ROD
+        val BoneSoundType = Sound.ITEM_BONE_MEAL_USE
+        val FullGrowthSoundType = Sound.ENTITY_PLAYER_LEVELUP
+    }
+
+    fun boneParticl(player: Player, location: Location, count: Int, offsetX: Double, offsetY: Double, offsetZ: Double, speed: Double) {
+        if (BoneParticle) {
+            player.world.spawnParticle(BoneParticleType, location, count, offsetX, offsetY, offsetZ, speed)
+        }
+    }
+
+    fun completeGrowthParticle(player: Player, location: Location, count: Int, offsetX: Double, offsetY: Double, offsetZ: Double, speed: Double) {
+        if (CompleteGrowthParticle) {
+            player.world.spawnParticle(CompleteGrowthParticleType, location, count, offsetX, offsetY, offsetZ, speed)
+        }
+    }
+
+    fun boneSound(player: Player, location: Location, volume: Float, pitch: Float) {
+        if (BoneSound) {
+            player.playSound(location, BoneSoundType, volume, pitch)
+        }
+    }
+
+    fun fullGrowthSound(player: Player, location: Location, volume: Float, pitch: Float) {
+        if (FullGrowthSound) {
+            player.playSound(location, FullGrowthSoundType, volume, pitch)
+        }
+    }
+}
