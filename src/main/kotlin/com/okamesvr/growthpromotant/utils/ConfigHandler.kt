868a2c078821740fc14d9d13c6034cd2088ae3a9
okame-486 <okame.aka.k3@gmail.com>
2024-01-20 14:05:05 +0900

    ConfigHandlerをリファクタリングして共通のプロパティを複数のクラスに適用する - ConfigHandlerクラスでは、loadConfigメソッドをリファクタリングして、設定ファイルから共通のプロパティを読み込んで複数のクラスに適用するように変更

--- src/main/kotlin/com/okamesvr/growthpromotant/utils/ConfigHandler.kt
+++ src/main/kotlin/com/okamesvr/growthpromotant/utils/ConfigHandler.kt
@@ -1,16 +1,38 @@
 package com.okamesvr.growthpromotant.utils
 
 import com.okamesvr.growthpromotant.SneakListener
+import com.okamesvr.growthpromotant.growthProcess.Farmland
+import com.okamesvr.growthpromotant.growthProcess.Sapling
+import com.okamesvr.growthpromotant.growthProcess.Sugarcane
 import org.bukkit.configuration.file.FileConfiguration
 
 class ConfigHandler(private var config: FileConfiguration) {
     fun loadConfig() {
-        SneakListener.PROBABILITY = config.getDouble("PROBABILITY")
+        // 共通のプロパティを読み込む
+        val probability = config.getDouble("PROBABILITY")
+        val boneParticle = config.getBoolean("BoneParticle")
+        val completeGrowthParticle = config.getBoolean("CompleteGrowthParticle")
+        val boneSound = config.getBoolean("BoneSound")
+        val fullGrowthSound = config.getBoolean("FullGrowthSound")
+
+        // 耕地に適用する
+        Farmland.PROBABILITY = probability
+
+        // サトウキビに適用する
+        Sugarcane.PROBABILITY = probability
+
+        // 苗木に適応する
+        Sapling.PROBABILITY = probability
+
+        // EffectsLibに適応する
+        EffectsLib.BoneParticle = boneParticle
+        EffectsLib.CompleteGrowthParticle = completeGrowthParticle
+        EffectsLib.BoneSound = boneSound
+        EffectsLib.FullGrowthSound = fullGrowthSound
+
+        // その他
         CooldownHandler.COOLTIME = config.getDouble("COOLTIME").toInt()
-        SneakListener.BoneParticle = config.getBoolean("BoneParticle")
-        SneakListener.CompleteGrowthParticle = config.getBoolean("CompleteGrowthParticle")
-        SneakListener.BoneSound = config.getBoolean("BoneSound")
-        SneakListener.FullGrowthSound = config.getBoolean("FullGrowthSound")
         FarmProtect.FarmProtect = config.getBoolean("FarmProtect")
     }
+
 }
